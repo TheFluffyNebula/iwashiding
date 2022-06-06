6e2f5f853e71d800a470d306c9c753f54380078a
nouturnsign <ericwangyy@gmail.com>
2022-06-06 15:41:26 -0400

    Replacing json idea

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -2,10 +2,9 @@ import os as _os
 from dotenv import load_dotenv as _load_dotenv
 import discord as _discord
 from discord.ext import commands as _commands
-import json as _json
 
-with open("emotes.json") as f:
-   emotes = _json.load(f)["emotes"]
+emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/3.0"}
+
 bot = _commands.Bot(command_prefix=";")
 _load_dotenv()
 DISCORD_TOKEN = _os.environ['DISCORD_TOKEN']
