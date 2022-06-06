94004e2e6c1d52ff466a508ccdc81f9c7b916307
nouturnsign <ericwangyy@gmail.com>
2022-06-06 19:50:31 -0400

    Gave demo and emote functionality again

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -4,11 +4,12 @@ from dotenv import load_dotenv as _load_dotenv
 import discord as _discord
 from discord.ext import commands as _commands
 
+COMMAND_PREFIX = ";"
 emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/3.0",
           ":)":"https://static-cdn.jtvnw.net/emoticons/v2/1/static/light/1.0",
           ":(":"https://static-cdn.jtvnw.net/emoticons/v2/555555558/static/light/1.0",
           ":O":"https://static-cdn.jtvnw.net/emoticons/v2/555555580/static/light/1.0"}
-bot = _commands.Bot(command_prefix=";")
+bot = _commands.Bot(command_prefix=COMMAND_PREFIX)
 
 _load_dotenv()
 DISCORD_TOKEN = _os.environ['DISCORD_TOKEN']
@@ -17,6 +18,13 @@ DISCORD_TOKEN = _os.environ['DISCORD_TOKEN']
 async def on_message(message):
     
     if message.author == bot.user:
+        return
+    
+    if message.content.startswith(COMMAND_PREFIX + 'demo'):
+        await demo(await bot.get_context(message))
+        return
+    if message.content.startswith(COMMAND_PREFIX + 'emote '):
+        await emote(await bot.get_context(message), message.content[message.content.index(' ') + 1:])
         return
     
     potential_emotes = _re.findall(r"(:(?:\w|[0-9])+:)", message.content)
