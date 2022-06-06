d3e87598475f2f9cca5f942cbebc252d7073290b
nouturnsign <ericwangyy@gmail.com>
2022-06-06 14:22:15 -0400

    Added demo

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -3,7 +3,12 @@ from dotenv import load_dotenv as _load_dotenv
 import discord as _discord
 from discord.ext import commands as _commands
 
-bot = _commands.Bot(command_prefix=">")
+bot = _commands.Bot(command_prefix=";")
 _load_dotenv()
 DISCORD_TOKEN = _os.environ['DISCORD_TOKEN']
+
+@bot.command()
+async def demo(ctx):
+    await ctx.send('Looks like things are working?')
+    
 bot.run(DISCORD_TOKEN)
