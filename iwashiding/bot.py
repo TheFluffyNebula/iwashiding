8af08033cccaf35fe3752fec5159e2ba1ec72e4e
TheFluffyNebula <jasonyin30@gmail.com>
2022-06-06 15:59:52 -0400

    added smiley and frown

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv as _load_dotenv
 import discord as _discord
 from discord.ext import commands as _commands
 
-emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/3.0"}
-
+emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/3.0",
+          ":)":"https://static-cdn.jtvnw.net/emoticons/v2/1/static/light/1.0",
+          ":(":"https://static-cdn.jtvnw.net/emoticons/v2/555555558/static/light/1.0"}
 bot = _commands.Bot(command_prefix=";")
+
 _load_dotenv()
 DISCORD_TOKEN = _os.environ['DISCORD_TOKEN']
 
