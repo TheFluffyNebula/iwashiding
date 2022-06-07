07f70c2eae72ac22d6aec3c92089eb55b33b89f8
TheFluffyNebula <jasonyin30@gmail.com>
2022-06-06 21:38:45 -0400

    added main emotes

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -9,7 +9,12 @@ COMMAND_PREFIX = ";"
 emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/1.0",
           ":)":"https://static-cdn.jtvnw.net/emoticons/v2/1/static/light/1.0",
           ":(":"https://static-cdn.jtvnw.net/emoticons/v2/555555558/static/light/1.0",
-          ":O":"https://static-cdn.jtvnw.net/emoticons/v2/555555580/static/light/1.0"}
+          ":O":"https://static-cdn.jtvnw.net/emoticons/v2/555555580/static/light/1.0",
+          "BibleThump":"https://static-cdn.jtvnw.net/emoticons/v2/86/static/light/1.0",
+          ":D":"https://static-cdn.jtvnw.net/emoticons/v2/3/static/light/1.0",
+          ":P":"https://static-cdn.jtvnw.net/emoticons/v2/12/static/light/1.0",
+          ":|":"https://static-cdn.jtvnw.net/emoticons/v2/555555563/static/light/1.0",
+          "<3":"https://static-cdn.jtvnw.net/emoticons/v2/555555584/static/light/1.0"}
 bot = _commands.Bot(command_prefix=COMMAND_PREFIX)
 
 _load_dotenv()
