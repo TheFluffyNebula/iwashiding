c7aa77742f173dc24e9eade4a1bb73aece553911
nouturnsign <ericwangyy@gmail.com>
2022-06-07 21:17:54 -0400

    Refactored presets/constants

--- iwashiding/bot.py
+++ iwashiding/bot.py
@@ -5,20 +5,8 @@ from dotenv import load_dotenv as _load_dotenv
 from discord.ext import commands as _commands
 import discord as _discord
 
-BOT_NAME = "IWasHiding"
-SEP = "_"
-BOT_DESCRIPTION = "Use popular twitch.tv emotes in discord!"
-COMMAND_PREFIX = ";"
-emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/1.0",
-          ":)":"https://static-cdn.jtvnw.net/emoticons/v2/1/static/light/1.0",
-          ":(":"https://static-cdn.jtvnw.net/emoticons/v2/555555558/static/light/1.0",
-          ":O":"https://static-cdn.jtvnw.net/emoticons/v2/555555580/static/light/1.0",
-          "BibleThump":"https://static-cdn.jtvnw.net/emoticons/v2/86/static/light/1.0",
-          ":D":"https://static-cdn.jtvnw.net/emoticons/v2/3/static/light/1.0",
-          ":P":"https://static-cdn.jtvnw.net/emoticons/v2/12/static/light/1.0",
-          ":|":"https://static-cdn.jtvnw.net/emoticons/v2/555555563/static/light/1.0",
-          "<3":"https://static-cdn.jtvnw.net/emoticons/v2/555555584/static/light/1.0"}
-TOP_EMOTES_URL = 'https://raw.githubusercontent.com/nouturnsign/iwashiding/master/top_emotes.json'
+from presets import *
+
 emotes.update(_requests.get(TOP_EMOTES_URL).json()['emotes'])
 bot = _commands.Bot(
     command_prefix=COMMAND_PREFIX, 
---
+++ iwashiding/presets.py
@@ -0,0 +1,14 @@
+BOT_NAME = "IWasHiding"
+SEP = "_"
+BOT_DESCRIPTION = "Use popular twitch.tv emotes in discord!"
+COMMAND_PREFIX = ";"
+emotes = {"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v2/305954156/static/light/1.0",
+          ":)":"https://static-cdn.jtvnw.net/emoticons/v2/1/static/light/1.0",
+          ":(":"https://static-cdn.jtvnw.net/emoticons/v2/555555558/static/light/1.0",
+          ":O":"https://static-cdn.jtvnw.net/emoticons/v2/555555580/static/light/1.0",
+          "BibleThump":"https://static-cdn.jtvnw.net/emoticons/v2/86/static/light/1.0",
+          ":D":"https://static-cdn.jtvnw.net/emoticons/v2/3/static/light/1.0",
+          ":P":"https://static-cdn.jtvnw.net/emoticons/v2/12/static/light/1.0",
+          ":|":"https://static-cdn.jtvnw.net/emoticons/v2/555555563/static/light/1.0",
+          "<3":"https://static-cdn.jtvnw.net/emoticons/v2/555555584/static/light/1.0"}
+TOP_EMOTES_URL = 'https://raw.githubusercontent.com/nouturnsign/iwashiding/master/top_emotes.json'
